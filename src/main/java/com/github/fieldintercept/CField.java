551cf1bbdd50df2971bd580188a6c54c1eb5813c
wangzihaogithub <842156727@qq.com>
2021-12-22 11:00:32 +0800

    修复使用valueField表达式注入集合时,需要根据泛型转换类型

--- src/main/java/com/github/fieldintercept/CField.java
+++ src/main/java/com/github/fieldintercept/CField.java
@@ -225,6 +225,9 @@ public class CField {
                     Map value2Map = BeanMap.toMap(value);
                     for (int i = 1; i < keys.length; i++) {
                         value = value2Map.get(keys[i]);
+                        if (i != keys.length - 1) {
+                            value2Map = BeanMap.toMap(value);
+                        }
                     }
                     return value == null ? null : String.valueOf(value);
                 }
