1cb2807bb34c46b4873ef42e74f84a2d9783b2c5
wangzihaogithub <hao.wang@iterget.com>
2023-03-13 00:33:50 +0800

    1.0.4 支持将N毫秒内的所有线程聚合到一起查询

--- src/test/java/com/github/fieldintercept/MainTest.java
+++ src/test/java/com/github/fieldintercept/MainTest.java
@@ -3,9 +3,18 @@ package com.github.fieldintercept;
 import com.github.fieldintercept.entity.Const;
 import com.github.fieldintercept.entity.Department;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MainTest {
     public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
         ReturnFieldDispatchAop dispatchAop = new ReturnFieldDispatchAop(Const.BEAN_FACTORY);
+        dispatchAop.setBatchAggregation(true);
+        dispatchAop.setBatchAggregationTimeMs(1000);
+        dispatchAop.setTaskExecutor(executorService::submit);
 
         for (int i = 0; i < 100; i++) {
             new Thread(){
@@ -22,7 +31,7 @@ public class MainTest {
         while (true) {
             Department department = new Department();
             dispatchAop.autowiredFieldValue(department);
-            System.out.println("department = " + department);
+            System.out.println("111department = " + department);
         }
 
 //        System.out.println("department = " + department);
